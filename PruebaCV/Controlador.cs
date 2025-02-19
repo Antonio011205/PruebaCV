057f909398f5b3e70f078ff35351d87437a2a07f
Antonio011205 <antonioperez011205@gmail.com>
2025-02-19 08:49:18 +0100

    estructura organizativa del proyecto: Clases instanciables y estaticas

---
+++ PruebaCV/Controlador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaCV
+{
+    /// <summary>
+    /// Clase estatica que sirve de intermediaria entre el modelo y las vistas de la App
+    /// </summary>
+    public static class Controlador
+    {
+    }
+}
---
+++ PruebaCV/Vehiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaCV
+{
+    /// <summary>
+    /// clase instanciable - Se pueden almacenar en memoria
+    /// </summary>
+    public class Vehiculo
+    {
+    }
+}
